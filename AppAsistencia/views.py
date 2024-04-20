d4c6d8b1967812187f02125badc543332d18bdae
LenixPantoja <aldair.pantoja.velasquez@gmail.com>
2024-04-19 23:55:11 -0500

    se modifica api asistencia estudiantes

--- AppAsistencia/views.py
+++ AppAsistencia/views.py
@@ -202,33 +202,22 @@ class AppAsist_API_AsistenciaEst(APIView):
                     else:
                         # Si no es un campo de tipo FileField o ImageField, asumimos que es la ruta de la imagen
                         soporte_url = dataAsistencia.soporte
-                print(id_estudiante == int(pIdEstudiante))
-                print(f"idinternoest: {id_estudiante}", {int(pIdEstudiante)})
-                print(f"idinternoest: {id_estudiante}", {int(pIdEstudiante)})
-
-                print(id_materia == int(pIdMateria))
-                print(f"idinternomater: {id_materia}", {int(pIdMateria)})
-                print(f"idinternomater: {id_materia}", {int(pIdMateria)})
-
-                print(id_curso == int(pIdCurso))      
-                print(f"idinternocurs: {id_curso}",{int(pIdCurso)})
-                print(f"idinternocurs: {id_curso}", {int(pIdCurso)})
-
+    
                 if (id_estudiante == int(pIdEstudiante) and
                     id_materia == int(pIdMateria) and
                     id_curso == int(pIdCurso)):
                     lista_asistencia.append({
-                        'id': dataAsistencia.id,
-                        'Tipo_asistencia': dataAsistencia.tipo_asistencia,
-                        'Descripcion_asistencia': dataAsistencia.descripcion,
-                        'Hora_llegada' : dataAsistencia.hora_llegada,
-                        'Soporte': soporte_url,
-                        'id_estudiante': dataAsistencia.matricula_estudiante.estudiante.id,
-                        'Estudiante': dataAsistencia.matricula_estudiante.estudiante.user.first_name + ' ' + dataAsistencia.matricula_estudiante.estudiante.user.last_name,
-                        'id_curso':dataAsistencia.matricula_estudiante.curso_Materia.curso.id,
-                        'Curso': dataAsistencia.matricula_estudiante.curso_Materia.curso.nombre_curso,
-                        'id_materia': dataAsistencia.matricula_estudiante.curso_Materia.materia.id,
-                        'Materia': dataAsistencia.matricula_estudiante.curso_Materia.materia.nombre_materia
+                        "id": dataAsistencia.id,
+                        "Tipo_asistencia": dataAsistencia.tipo_asistencia,
+                        "Descripcion_asistencia": dataAsistencia.descripcion,
+                        "Hora_llegada": dataAsistencia.hora_llegada,
+                        "Soporte": soporte_url,
+                        "id_estudiante": dataAsistencia.matricula_estudiante.estudiante.id,
+                        "Estudiante": dataAsistencia.matricula_estudiante.estudiante.user.first_name + ' ' + dataAsistencia.matricula_estudiante.estudiante.user.last_name,
+                        "id_curso":dataAsistencia.matricula_estudiante.curso_Materia.curso.id,
+                        "Curso": dataAsistencia.matricula_estudiante.curso_Materia.curso.nombre_curso,
+                        "id_materia": dataAsistencia.matricula_estudiante.curso_Materia.materia.id,
+                        "Materia": dataAsistencia.matricula_estudiante.curso_Materia.materia.nombre_materia
                     })
                     print(lista_asistencia)
             return Response(lista_asistencia)
