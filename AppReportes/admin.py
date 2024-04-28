b279a398f8b769fa441093e7194c5861eac95707
LenixPantoja <aldair.pantoja.velasquez@gmail.com>
2024-04-27 23:42:53 -0500

    Se agrega modelo y api de notificaciones

--- AppReportes/admin.py
+++ AppReportes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from AppReportes.models import Notification
 # Register your models here.
+
+@admin.register(Notification)
+class NotificacionAdmin(admin.ModelAdmin):
+    list_filter = ('message', 'created_at', 'updated_at',)
+    list_display = ('message', 'created_at', 'updated_at',)
+    search_fields = ('message', 'created_at', 'updated_at',)
+    ordering = ('message',)
